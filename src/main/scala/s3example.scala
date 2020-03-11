08d61dd15702206f9023869da6c7a4a1dd4ef2ae
siddhusalvi <siddheshsalvi525@gmail.com>
2020-03-11 10:45:50 +0530

    REFACTORED: aws keys

--- src/main/scala/s3example.scala
+++ src/main/scala/s3example.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 
 object s3example extends App {
 
-  val cred = new BasicAWSCredentials("AKIAJF2DZFGGEYETALOQ", "wSCnvoupSglH3P19qkiFVWSKrg+Ho/RcFxFrmRAG")
+  val cred = new BasicAWSCredentials("", "")
   val s3Client = new AmazonS3Client(cred)
   val s3Object = s3Client.getObject(new GetObjectRequest("sparkweek6", "words.csv"))
   val myData = Source.fromInputStream(s3Object.getObjectContent).getLines()
